a8ccd921c39abbe4ed213c95d01ef1e7f06d1086
Цылиакус Максм <tsyliakusmaxim@gmail.com>
2019-12-01 22:07:00 +0300

    - заготовка под клиентскую часть

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RolesComponent } from './roles/roles.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/reg/reg.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RolesComponent,
+    UserComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/reg/reg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reg',
+  templateUrl: './reg.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
